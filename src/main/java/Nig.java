f3552d9cdbb5d1c6708889833587582b37c5d29f
Bryan <178081756+bryankuah@users.noreply.github.com>
2026-01-29 11:58:53 +0800

    Added Echo function to NIG

--- src/main/java/Nig.java
+++ src/main/java/Nig.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class Nig {
     public static void main(String[] args) {
         String logo = "____________________________________________________________\n"
                 + " Hello! I'm NIG\n"
                 + " What can I do for you?\n"
-                + "____________________________________________________________\n"
+                + "____________________________________________________________\n";
+
+        String goodbye = "____________________________________________________________\n"
                 + " Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________\n";
-        System.out.println("Hello from\n" + logo);
+
+        String separator = "____________________________________________________________\n";
+
+        System.out.println(logo);
+
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                break;
+            }
+            System.out.println(separator + " " + line + "\n" + separator);
+        }
+
+        System.out.println(goodbye);
     }
 }
