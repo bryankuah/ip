bedbf5a9081450a9198776ec69421acc0fee20b8
Bryan <178081756+bryankuah@users.noreply.github.com>
2026-02-02 18:33:01 +0800

    Tweak code to comply with coding standards

--- src/main/java/Nig.java
+++ src/main/java/Nig.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class Nig {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
+        String LOGO = "____________________________________________________________\n"
                 + " Hello! I'm NIG\n"
                 + " What can I do for you?\n"
                 + "____________________________________________________________\n";
 
-        String goodbye = "____________________________________________________________\n"
+        String GOODBYE = "____________________________________________________________\n"
                 + " Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________\n";
 
-        String separator = "____________________________________________________________\n";
+        String SEPARATOR = "____________________________________________________________\n";
 
-        System.out.println(logo);
+        System.out.println(LOGO);
 
         String line;
         Scanner in = new Scanner(System.in);
@@ -27,37 +27,37 @@ public class Nig {
                 break;
             }
             if (line.equals("list")) {
-                System.out.println(separator);
+                System.out.println(SEPARATOR);
                 for (int i = 0; i < taskCount; i++) {
                     Task currentTask = tasks[i];
                     System.out.print(" " + (i + 1) + ". ");
                     System.out.print("[" + currentTask.getStatusIcon() + "] ");
                     System.out.println(currentTask.getDescription());
                 }
-                System.out.println(separator);
+                System.out.println(SEPARATOR);
             } else if (line.split(" ")[0].equals("mark")) {
                 int taskNumber = Integer.parseInt(line.split(" ")[1]);
                 Task taskToMark = tasks[taskNumber - 1];
                 taskToMark.markAsDone();
-                System.out.println(separator);
+                System.out.println(SEPARATOR);
                 System.out.println(" Nice! I've marked this task as done:");
                 System.out.println("   [X] " + taskToMark.getDescription());
-                System.out.println(separator);
+                System.out.println(SEPARATOR);
             } else if (line.split(" ")[0].equals("unmark")) {
                 int taskNumber = Integer.parseInt(line.split(" ")[1]);
                 Task taskToMark = tasks[taskNumber - 1];
                 taskToMark.markAsUndone();
-                System.out.println(separator);
+                System.out.println(SEPARATOR);
                 System.out.println(" OK, I've marked this task as not done yet:");
                 System.out.println("   [ ] " + taskToMark.getDescription());
-                System.out.println(separator);
+                System.out.println(SEPARATOR);
             } else {
                 tasks[taskCount] = new Task(line);
                 taskCount++;
-                System.out.println(separator + " added: " + line + "\n" + separator);
+                System.out.println(SEPARATOR + " added: " + line + "\n" + SEPARATOR);
             }
         }
 
-        System.out.println(goodbye);
+        System.out.println(GOODBYE);
     }
 }
