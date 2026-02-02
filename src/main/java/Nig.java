3aa39bf993377ca1ec38c549750a31619049acee
Bryan <178081756+bryankuah@users.noreply.github.com>
2026-02-02 18:20:48 +0800

    Add the ability to mark tasks as done or undone
    Created a Task class to track "isDone" for each object
    Nig chatbot now looks out for "mark" or "unmark" and will set isDone
    to true or false for the respective task number

--- src/main/java/Nig.java
+++ src/main/java/Nig.java
@@ -18,7 +18,7 @@ public class Nig {
         String line;
         Scanner in = new Scanner(System.in);
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         while (true) {
@@ -29,11 +29,30 @@ public class Nig {
             if (line.equals("list")) {
                 System.out.println(separator);
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    Task currentTask = tasks[i];
+                    System.out.print(" " + (i + 1) + ". ");
+                    System.out.print("[" + currentTask.getStatusIcon() + "] ");
+                    System.out.println(currentTask.getDescription());
                 }
                 System.out.println(separator);
+            } else if (line.split(" ")[0].equals("mark")) {
+                int taskNumber = Integer.parseInt(line.split(" ")[1]);
+                Task taskToMark = tasks[taskNumber - 1];
+                taskToMark.markAsDone();
+                System.out.println(separator);
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   [X] " + taskToMark.getDescription());
+                System.out.println(separator);
+            } else if (line.split(" ")[0].equals("unmark")) {
+                int taskNumber = Integer.parseInt(line.split(" ")[1]);
+                Task taskToMark = tasks[taskNumber - 1];
+                taskToMark.markAsUndone();
+                System.out.println(separator);
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   [ ] " + taskToMark.getDescription());
+                System.out.println(separator);
             } else {
-                tasks[taskCount] = line;
+                tasks[taskCount] = new Task(line);
                 taskCount++;
                 System.out.println(separator + " added: " + line + "\n" + separator);
             }
